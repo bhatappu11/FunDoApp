2df09fe8342d34bd340be09d1267bc625b1a6cea
Arpitha Bhat <arpithabht12@gmail.com>
2021-11-06 13:26:15 +0530

    [Arpitha]REFACTORED: added path to the signin page and included its component

--- fundoapp/src/App.js
+++ fundoapp/src/App.js
@@ -1,10 +1,12 @@
 import SignUp from "./pages/signup/SignUp"
+import SignIn from "./pages/signin/SignIn"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <SignUp />
+      {/* <SignUp /> */}
+      <SignIn />
     </div>
   );
 }
