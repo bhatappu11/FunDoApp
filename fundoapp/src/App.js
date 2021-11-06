1821bbd585f2acf8c1d000b65e60f4464c5dab14
Arpitha Bhat <arpithabht12@gmail.com>
2021-11-06 15:13:15 +0530

    [Arpitha]REFACTORED: added path to the forgot page and called its component

--- fundoapp/src/App.js
+++ fundoapp/src/App.js
@@ -1,12 +1,14 @@
 import SignUp from "./pages/signup/SignUp"
 import SignIn from "./pages/signin/SignIn"
+import Forgot from "./pages/forgot/Forgot"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       {/* <SignUp /> */}
-      <SignIn />
+      {/* <SignIn /> */}
+      <Forgot />
     </div>
   );
 }
