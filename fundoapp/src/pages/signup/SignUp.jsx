721f3a8ce883997dd58e021112fa364aefa099b2
Arpitha Bhat <arpithabht12@gmail.com>
2021-11-08 15:06:32 +0530

    [Arpitha]ADDED: validation method for user inputs

--- fundoapp/src/pages/signup/SignUp.jsx
+++ fundoapp/src/pages/signup/SignUp.jsx
@@ -3,8 +3,65 @@ import './SignUp.scss';
 import TextField from '@mui/material/TextField';
 import Checkbox from '@mui/material/Checkbox';
 import Button from '@mui/material/Button';
+import UserService from '../../services/UserService';
+const userService = new UserService();
 
 export default class SignUp extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            fName: "",
+            lName: "",
+            email: "",
+            password: "",
+            fNameError: false,
+            lNameError: false,
+            emailError: false,
+            passwordError: false,
+        };
+    }
+    isValidated = () => {
+        let isError = false;
+        const errors = this.state;
+
+        errors.fNameError = this.state.fName !== "" ? false : true;
+        errors.lNameError = this.state.lName !== "" ? false : true;
+        errors.emailError = this.state.email !== "" ? false : true;
+        errors.passwordError = this.state.password !== "" ? false : true;
+
+        this.setState({
+            ...errors
+        });
+        return (isError = errors.fNameError || errors.lNameError || errors.emailError || errors.passwordError);
+    };
+
+    changeValue = (e) => {
+        this.setState({
+          [e.target.name]: e.target.value,
+        });
+      };
+
+    next = () => {
+        var isValid = this.isValidated();
+        console.log(this.state)
+        if(!isValid){
+            console.log("validation successful");
+            let data = {
+                "firstName": this.state.fName,
+                "lastName": this.state.lName,
+                "email":this.state.email,
+                "password":this.state.password,
+                "service":"advance",
+            };
+            userService.SignUp("/user/userSignUp",data)
+            .then(()=>{
+                console.log("successfully registered");
+            })
+            .catch((err)=>{
+                console.log(err);
+            });
+        }
+    };
     render() {
         return (
             <div className="signup-container">
@@ -19,18 +76,34 @@ export default class SignUp extends Component {
                     <h1 className="form-heading">Create your Google Account</h1>
                     <div className="form">
                         <div className="name-field">
-                            <TextField fullwidth id="first-name" label="First name" variant="outlined" margin="dense" size="small" sx={{ marginRight: '5px' }}/>
-                            <TextField fullwidth id="last-name" label="Last name" variant="outlined" margin="dense" size="small" sx={{ marginLeft: '5px' }}/>
+                            <TextField fullwidth id="first-name" name="fName" label="First name" variant="outlined" margin="dense" 
+                                        size="small" error={this.state.fNameError}
+                                        onChange={(e) => this.changeValue(e)}
+                                        helperText={this.state.fNameError ? "Enter first name" : ""}
+                                         sx={{ marginRight: '5px' }}/>
+                            <TextField fullwidth id="last-name" name="lName" label="Last name" variant="outlined" margin="dense" 
+                                        size="small" error={this.state.lNameError} 
+                                        onChange={(e) => this.changeValue(e)}
+                                        helperText={this.state.lNameError ? "Enter last name" : ""}
+                                        sx={{ marginLeft: '5px' }}/>
                         </div>
                         <div className="email-field">
-                            <TextField fullwidth id="email" label="Username" variant="outlined" margin="dense" size="small" helperText="You can use letters,numbers & periods" sx={{width: '100%'}}/>
+                            <TextField fullwidth id="email" name="email" label="Username" variant="outlined" margin="dense" 
+                                        error={this.state.emailError}
+                                        onChange={(e) => this.changeValue(e)}
+                                        helperText={this.state.emailError ? "choose a Gmail address" : ""}
+                                        size="small" helperText="You can use letters,numbers & periods" sx={{width: '100%'}}/>
                         </div>
                         <div className="current-email">
                             <Button className="email-button" size="small" sx={{textTransform: 'none', fontWeight: 'bolder', fontSize: '0.875rem'}}>Use my current email address instead</Button>
                         </div>
                         <div className="password-field">
-                            <TextField fullwidth id="password" label="Password" variant="outlined" margin="dense" type="password" size="small" sx={{ marginRight: '5px' }}/>
-                            <TextField fullwidth id="password" label="Confirm" variant="outlined" margin="dense" type="password" size="small" sx={{ marginRight: '5px' }}/>
+                            <TextField fullwidth id="password" name="password" label="Password" variant="outlined" margin="dense" type="password" 
+                                        error={this.state.passwordError}
+                                        onChange={(e) => this.changeValue(e)}
+                                        helperText={this.state.passwordError ? "Enter a password" : ""}
+                                        size="small" sx={{ marginRight: '5px' }}/>
+                            <TextField fullwidth id="cpassword" label="Confirm" variant="outlined" margin="dense" type="password" size="small" sx={{ marginRight: '5px' }}/>
                             
                         </div>
                         <div className="password-helpertext">Use 8 or more characters with a mix of letters, numbers & symbols</div>
@@ -39,7 +112,7 @@ export default class SignUp extends Component {
                         </div>
                         <div className="signin-button">
                             <Button size="small" sx={{textTransform: 'none', fontWeight: 'bolder', fontSize: '0.875rem', marginRight:'8vw'}}>Sign in instead</Button>
-                            <Button size="medium" variant="contained" sx={{textTransform: 'none', fontWeight: 'bolder', fontSize: '0.875rem'}}>Next</Button>
+                            <Button size="medium" variant="contained" sx={{textTransform: 'none', fontWeight: 'bolder', fontSize: '0.875rem'}} onClick={this.next}>Next</Button>
                         </div>
                     </div>
                 </div>
