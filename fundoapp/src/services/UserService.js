3e5558d69054a21a153cbf1aa313a9151080e84b
Arpitha Bhat <arpithabht12@gmail.com>
2021-11-08 19:08:48 +0530

    [Arpitha]ADDED: methods to call axios service based on the user requirement

--- fundoapp/src/services/UserService.js
+++ fundoapp/src/services/UserService.js
@@ -5,7 +5,16 @@ let baseurl = 'http://fundoonotes.incubation.bridgelabz.com/api'
 
 class UserService {
     SignUp(url,data){
-       return axiosService.postMethod(`${baseurl}${url}`,data);
+        return axiosService.postMethod(`${baseurl}${url}`,data);
+    }
+    SignIn(url,data){
+        return axiosService.postMethod(`${baseurl}${url}`,data);
+    }
+    Forgot(url,data){
+        return axiosService.postMethod(`${baseurl}${url}`,data);
+    }
+    Reset(url,data,config){
+        return axiosService.postMethodForReset(`${baseurl}${url}`,data,config);
     }
 }
 
