c0ce05fbf083c7fa92275beac8dd1a1e7255ff82
Arpitha Bhat <arpithabht12@gmail.com>
2021-11-08 19:08:06 +0530

    [Arpitha]ADDED: method to call api for all the pages

--- fundoapp/src/services/AxiosService.js
+++ fundoapp/src/services/AxiosService.js
@@ -4,6 +4,9 @@ class AxiosService {
     postMethod(url,data){
         return axios.post(url,data)
     }
+    postMethodForReset(url,data,config){
+        return axios.post(url,data,config)
+    }
 }
 
 export default AxiosService
