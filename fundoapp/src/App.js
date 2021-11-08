73aa0b384a1e24b9add3605ca13221c4fdae722c
Arpitha Bhat <arpithabht12@gmail.com>
2021-11-08 15:16:37 +0530

    [Arpitha]ADDED: axios package

--- fundoapp/src/App.js
+++ fundoapp/src/App.js
@@ -7,10 +7,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      {/* <SignUp /> */}
+      <SignUp />
       {/* <SignIn /> */}
       {/* <Forgot /> */}
-      <Reset />
+      {/* <Reset /> */}
     </div>
   );
 }
