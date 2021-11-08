e7a50b96ee58b720be09b653410974bc0520082a
Arpitha Bhat <arpithabht12@gmail.com>
2021-11-08 19:06:57 +0530

    [Arpitha]ADDED: validations to input fields and api call

--- fundoapp/src/pages/forgot/Forgot.jsx
+++ fundoapp/src/pages/forgot/Forgot.jsx
@@ -2,9 +2,53 @@ import React, { Component } from 'react'
 import './Forgot.scss';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import UserService from '../../services/UserService';
+const userService = new UserService();
 
 
 export default class Forgot extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            email: "",
+            emailError: false,
+        };
+    }
+    isValidated = () => {
+        let isError = false;
+        const errors = this.state;
+
+        errors.emailError = this.state.email !== "" ? false : true;
+
+        this.setState({
+            ...errors
+        });
+        return (isError = errors.emailError);
+    };
+
+    changeValue = (e) => {
+        this.setState({
+          [e.target.name]: e.target.value,
+        });
+      };
+
+    next = () => {
+        var isValid = this.isValidated();
+        console.log(this.state)
+        if(!isValid){
+            console.log("validation successful");
+            let data = {
+                "email":this.state.email,
+            };
+            userService.Forgot("/user/reset",data)
+            .then(()=>{
+                console.log("successfully found email");
+            })
+            .catch((err)=>{
+                console.log(err);
+            });
+        }
+    };
     render() {
         return (
             <div className="forgot-container">
@@ -19,10 +63,14 @@ export default class Forgot extends Component {
                     <div className="forgot-content-heading">Find your email</div>
                     <div className="forgot-content-description">Enter your phone number or recovery email</div>
                     <div className="forgot-email-field">
-                        <TextField fullwidth id="emailid" label="Phone number or email" variant="outlined" margin="dense" size="medium" sx={{width: '100%'}} />
+                        <TextField fullwidth id="emailid" name="email" label="Phone number or email" variant="outlined" margin="dense"
+                        error={this.state.emailError}
+                        onChange={(e) => this.changeValue(e)}
+                        helperText={this.state.emailError ? "Enter an email address" : ""}
+                        size="medium" sx={{width: '100%'}} />
                     </div>
                     <div className="forgot-button">
-                        <Button size="medium" variant="contained" sx={{textTransform: 'none', fontWeight: 'bolder', fontSize: '0.875rem' ,float: 'right'}}>Next</Button>
+                        <Button size="medium" variant="contained" sx={{textTransform: 'none', fontWeight: 'bolder', fontSize: '0.875rem' ,float: 'right'}}onClick={this.next}>Next</Button>
                     </div>
                 </div>
             </div>
