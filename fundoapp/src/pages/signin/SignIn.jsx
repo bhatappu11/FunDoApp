6b287aa52ecfa9035197d7f69d35a03164c1229c
Arpitha Bhat <arpithabht12@gmail.com>
2021-11-08 19:05:34 +0530

    [Arpitha]ADDED: validations to the input fields and api post call

--- fundoapp/src/pages/signin/SignIn.jsx
+++ fundoapp/src/pages/signin/SignIn.jsx
@@ -2,8 +2,56 @@ import React, { Component } from 'react'
 import './SignIn.scss';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import UserService from '../../services/UserService';
+const userService = new UserService();
 
 export default class SignIn extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            email: "",
+            password: "",
+            emailError: false,
+            passwordError: false,
+        };
+    }
+    isValidated = () => {
+        let isError = false;
+        const errors = this.state;
+
+        errors.emailError = this.state.email !== "" ? false : true;
+        errors.passwordError = this.state.password !== "" ? false : true;
+
+        this.setState({
+            ...errors
+        });
+        return (isError = errors.emailError || errors.passwordError);
+    };
+
+    changeValue = (e) => {
+        this.setState({
+          [e.target.name]: e.target.value,
+        });
+      };
+
+    next = () => {
+        var isValid = this.isValidated();
+        console.log(this.state)
+        if(!isValid){
+            console.log("validation successful");
+            let data = {
+                "email":this.state.email,
+                "password":this.state.password,
+            };
+            userService.SignIn("/user/login",data)
+            .then(()=>{
+                console.log("Login successful");
+            })
+            .catch((err)=>{
+                console.log(err);
+            });
+        }
+    };
     render() {
         return (
             <div className="signin-container">
@@ -18,10 +66,21 @@ export default class SignIn extends Component {
                 <div className="page-heading">Sign in</div>
                 <div className="container-description">Use your FunDo Account</div>
                 <div className="email-field">
-                    <TextField fullwidth id="emailid" label="Email or Phone" variant="outlined" margin="dense" size="medium" sx={{width: '100%'}} />
+                    <TextField fullwidth id="emailid" name="email" label="Email or Phone" variant="outlined" margin="dense" 
+                        error={this.state.emailError}
+                        onChange={(e) => this.changeValue(e)}
+                        helperText={this.state.emailError ? "Enter an email address" : ""}
+                        size="medium" sx={{width: '100%'}} />
                 </div>
                 <div className="forgot-email">
                 <Button className="email-button" size="small" sx={{textTransform: 'none', fontWeight: 'bolder', fontSize: '0.875rem'}}>Forgot email?</Button>
+                </div>
+                <div className="signin-password-field">
+                    <TextField fullwidth id="password" name="password" label="Password" variant="outlined" margin="dense" type="password" 
+                            error={this.state.passwordError}
+                            onChange={(e) => this.changeValue(e)}
+                            helperText={this.state.passwordError ? "Enter a password" : ""}
+                            size="medium" sx={{ width: '100%'}}/>
                 </div>
                 <div className="learn-more">
                     <div className="guest-mode">Not your computer? Use Guest mode to sign in privately.</div>
@@ -29,7 +88,7 @@ export default class SignIn extends Component {
                 </div>
                 <div className="create-account-buttons">
                     <Button className="create-button" size="small" sx={{textTransform: 'none', fontWeight: 'bolder', fontSize: '0.875rem'}}>Create account</Button>
-                    <Button size="medium" variant="contained" sx={{textTransform: 'none', fontWeight: 'bolder', fontSize: '0.875rem' ,float: 'right'}}>Next</Button>
+                    <Button size="medium" variant="contained" sx={{textTransform: 'none', fontWeight: 'bolder', fontSize: '0.875rem' ,float: 'right'}}onClick={this.next}>Next</Button>
                 </div>
                 </div>
             </div>
