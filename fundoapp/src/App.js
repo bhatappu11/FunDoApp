a2c345d6d04fafd2f7a0a198f807f339279387f0
Arpitha Bhat <arpithabht12@gmail.com>
2021-11-10 23:15:12 +0530

    [Arpitha]REFACTORED: routing is added to route between pages

--- fundoapp/src/App.js
+++ fundoapp/src/App.js
@@ -4,16 +4,24 @@ import Forgot from "./pages/forgot/Forgot"
 import Reset from "./pages/reset/Reset"
 import Dashboard from "./pages/dashboard/Dashboard"
 import './App.css';
+import History from "./history/History"
+import { Router, Route, Switch} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      {/* <SignUp /> */}
-      {/* <SignIn /> */}
-      {/* <Forgot /> */}
-      {/* <Reset /> */}
-      <Dashboard />
-    </div>
+      
+        <div className="App">
+          <Router history={History}>
+          <Switch>
+            <Route exact path='/signup' component={SignUp}></Route>
+            <Route exact path='/' component={SignIn}></Route>
+            <Route exact path='/forgot' component={Forgot}></Route>
+            <Route exact path='/reset-password/:id' component={Reset}></Route>
+            <Route exact path='/dashboard' component={Dashboard}></Route>
+          </Switch>
+          </Router>
+        </div>
+      
   );
 }
 
