af72d87a548894c318222356aa6abab7fafb9878
Arpitha Bhat <arpithabht12@gmail.com>
2021-11-16 09:06:04 +0530

    [Arpitha]ADDED: nested routing to route to archive and trash

--- fundoapp/src/pages/dashboard/Dashboard.jsx
+++ fundoapp/src/pages/dashboard/Dashboard.jsx
@@ -2,37 +2,17 @@ import React, { Component ,useEffect, useState} from 'react'
 import Header from '../../components/header/Header'
 import './Dashboard.scss'
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
-import Box from '@mui/material/Box';
 import { experimentalStyled as styled } from '@mui/material/styles';
-
-import TakeNotes from '../../components/takenotes/TakeNotes';
-import Display from '../../components/display/Display';
 import UserService from '../../services/UserService';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Archives from '../archives/Archives'
+import Trash from '../trash/Trash'
+import Notes from '../notes/Notes'
 
 const userService = new UserService();
 
 export default function Dashboard() {
-    const [notes, setNotes] = useState([""]);
-    const displayNote = () => {
-        let config = {
-            headers: {
-                'Authorization' : localStorage.getItem("id"),
-            }
-        };
-        userService.displayNotes("/notes/getNotesList",config)
-        .then((res)=>{
-            const data = res.data;
-            const notes = data;
-            setNotes(notes.data.data);
-            console.log(notes.data.data);
-            console.log("Notes displayed");
-        })
-        .catch((err)=>{
-            console.log(err);
-        });
-    }
     const theme = createTheme({
         palette:{
             mode:'dark',
@@ -44,16 +24,15 @@ export default function Dashboard() {
         textAlign: 'center',
         color: theme.palette.text.secondary,
       }));
-
-    React.useEffect(()=>{
-         displayNote();
-    },[]);
     return (
         <div>
             <ThemeProvider theme={theme}>
             <Header />
-            <TakeNotes displayAfterAdd = {displayNote}/>
-            <Display data={notes} displayAfterUpdate = {displayNote}/> 
+            <Switch>
+                <Route exact path="/dashboard" component={Notes}/>
+                <Route exact  path="/dashboard/archive" component={Archives}/>
+                <Route exact path="/dashboard/trash" component={Trash}/>
+            </Switch>
             </ThemeProvider>
         </div>
     )
