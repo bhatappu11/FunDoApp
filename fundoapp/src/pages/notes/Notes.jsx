6befc11ceb975aac7ffe331708db8377d4813c4d
Arpitha Bhat <arpithabht12@gmail.com>
2021-11-20 20:32:45 +0530

    [Arpitha]ADDED: handles search mode and calling display component accordingly

--- fundoapp/src/pages/notes/Notes.jsx
+++ fundoapp/src/pages/notes/Notes.jsx
@@ -1,12 +1,13 @@
-import React,{ Component ,useEffect, useState} from 'react'
+import React,{ Component ,useContext,useEffect, useState} from 'react'
 import UserService from '../../services/UserService';
 import TakeNotes from '../../components/takenotes/TakeNotes';
 import Display from '../../components/display/Display';
 
 const userService = new UserService();
 
-export default function Notes() {
-    const [notes, setNotes] = useState([""]);
+export default function Notes(props) {
+    const [notes, setNotes] = useState([]);
+        
     const displayNote = () => {
         let config = {
             headers: {
@@ -16,7 +17,7 @@ export default function Notes() {
         userService.displayNotes("/notes/getNotesList",config)
         .then((res)=>{
             const data = res.data;
-            const notes = data;
+            let notes = data;
             setNotes(notes.data.data);
             console.log(notes.data.data);
             console.log("Notes displayed");
@@ -30,8 +31,13 @@ export default function Notes() {
    },[]);
     return (
         <div>
-            <TakeNotes displayAfterAdd = {displayNote}/>
-            <Display data={notes.filter(ele => ele.isArchived == false && ele.isDeleted == false)} displayAfterUpdate = {displayNote}/> 
+            { props.mode=="search" ? (props.searchWord.length != 0 ? <Display data={notes.filter(ele => ((ele.title.includes(props.searchWord) || ele.description.includes(props.searchWord))) && ele.isDeleted == false)} displayAfterUpdate = {displayNote}/> : '')
+                :
+                <div>
+                <TakeNotes displayAfterAdd = {displayNote}/>
+                <Display data={notes.filter(ele => ele.isArchived == false && ele.isDeleted == false)} displayAfterUpdate = {displayNote}/> 
+                </div>
+            }
         </div>
     )
 }
